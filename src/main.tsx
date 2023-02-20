9c6935aef30ece1ba7e67a53daa4481c2ed7b3ad
MrBurp <andreass.ziegler@gmail.com>
2023-02-20 17:24:53 +0100

    updating  aside

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,11 @@ import "./global.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar">Sidebar</aside>
+      <aside className="sidebar">
+        <img src="" alt="Logo" />
+        <nav></nav>
+        <button>Tweet</button>
+      </aside>
       <div className="content">Content</div>
     </div>
   </React.StrictMode>
