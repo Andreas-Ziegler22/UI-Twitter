f94c715404105a8f08a09d296f6f4c796e766745
MrBurp <andreass.ziegler@gmail.com>
2023-02-20 17:12:56 +0100

    classname layout update

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import "./global.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar"></aside>
-      <div className="content"></div>
+      <aside className="sidebar">Sidebar</aside>
+      <div className="content">Content</div>
     </div>
   </React.StrictMode>
 );
