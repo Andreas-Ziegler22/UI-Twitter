3d387606e718f29507b9a3e18edc15701a7e8b6c
MrBurp <andreass.ziegler@gmail.com>
2023-02-20 17:05:36 +0100

    layout html

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,10 @@ import ReactDOM from "react-dom/client";
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <div className="layout">
+      <aside className="sidebar"></aside>
+      <div className="content"></div>
+    </div>
+  </React.StrictMode>
 );
