f5d0dbad0e8ddf8f0227ecd2b554a28ce8d5d6d3
MrBurp <andreass.ziegler@gmail.com>
2023-02-23 17:03:54 +0100

    update routes with children and Default outfit

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,10 +1,13 @@
+import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
 
 export function Default() {
   return (
     <div className="layout">
       <Sidebar />
-      <div className="content"></div>
+      <div className="content">
+        <Outlet />
+      </div>
     </div>
   );
 }
