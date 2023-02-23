d5d2826e41dedf344e26e6cc5139e343b5e107b1
MrBurp <andreass.ziegler@gmail.com>
2023-02-23 16:53:27 +0100

    uptdade main tsx and Default.tsx

---
+++ src/layouts/Default.tsx
@@ -0,0 +1,10 @@
+import { Sidebar } from "../components/Sidebar";
+
+export function Default() {
+  return (
+    <div className="layout">
+      <Sidebar />
+      <div className="content"></div>
+    </div>
+  );
+}
